9fb5339211737ff9f7f1a2520d90cd84aef22c55
eezn <pplaneoo@gmail.com>
2022-01-17 16:03:14 +0900

    init data_structure

---
+++ includes/data_structure.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   data_structure.h                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jin-lee <jin-lee@student.42seoul.kr>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/17 15:39:53 by jin-lee           #+#    #+#             */
+/*   Updated: 2022/01/17 15:42:07 by jin-lee          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DATA_STRUCTURE_H
+# define DATA_STRUCTURE_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "../libft/includes/libft.h"
+
+#endif
